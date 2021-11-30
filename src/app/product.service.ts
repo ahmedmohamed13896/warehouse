f8922a70b962be1c2dc891b357d86350ed849cca
ahmed mohamed <ahmedmohamed77765@hotmail.com>
2021-11-30 17:42:00 +0200

    fix validation and show filtered data in the table

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  url = "../assets/server/data.json";
+  url = "./assets/server/data.json";
   constructor(private http: HttpClient) { }
 
   getProductData(){
