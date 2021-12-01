814c6f99591e77da61d312d28feb7dc93650961b
Ahmed Mohamed Anwar <Anwar-Ahmed@andalusia.loc>
2021-12-01 11:33:24 +0200

    created product-table component

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -16,15 +16,14 @@ export class ProductComponent implements OnInit {
 
   filteredData:any= {};
 
-  // pagination
-  page:number =1;
+
 
 
   // Reactiveform
   formData = new FormGroup({
           warehouse : new FormControl({value:'',disabled: false},Validators.required),
           type : new FormControl({value:'',disabled: true},Validators.required),
-          showZeroBalance : new FormControl({value:true,disabled: false}),
+          showZeroBalance : new FormControl({value:false,disabled: true}),
           classification : new FormControl({value:'allProducts',disabled: true},Validators.required),
           products : new FormControl({value:[]},Validators.required),
         });
@@ -48,16 +47,18 @@ export class ProductComponent implements OnInit {
       return select?.target?.value == wh?.warhouse?.name
     })[0]?.warhouse || null;
 
-
+    this.formData.controls.type.patchValue('');
     if(this.selectedWarhouse !== null){
-        this.formData.controls.type.enable()
+        this.formData.controls.type.enable();
+        this.formData.controls.showZeroBalance.enable();
       }
     else{
         this.formData.controls.type.disable();
-        this.formData.controls.type.patchValue('');
+        this.formData.controls.showZeroBalance.disable();
       }
-
   }
+
+
   setCurrentTypes(type:any){
      this.productTypes = this.selectedWarhouse?.types?.filter((t:any)=>{
         return t?.type == type?.target?.value
@@ -73,32 +74,33 @@ export class ProductComponent implements OnInit {
     }
     this.formData.controls.classification.patchValue('allProducts');
 
-    }
 
-    showData(){
-      this.filteredData = this.formData.value;
-      console.log(this.formData.value);
-    }
+  }
 
-    checkIfNotSingle():boolean{
-      if(this.formData.controls.type.value != null && this.formData.controls.classification.value == 'specificProducts'){
-        return true;
-      }
-      if(this.formData.controls.classification.value == 'allProducts'){
-        return false
-      }
+  showData(){
+    this.filteredData = this.formData.value;
+    console.log(this.formData.value);
+  }
+
+  checkIfNotSingle():boolean{
+    if(this.formData.controls.type.value != null && this.formData.controls.classification.value == 'specificProducts'){
+      return true;
+    }
+    if(this.formData.controls.classification.value == 'allProducts'){
       return false
     }
+    return false
+  }
 
-    handleClassChange(){
-      this.checkIfNotSingle() ?
-      this.formData.controls.products.patchValue(this.productTypes):
-      this.formData.controls.products.patchValue([]);
-    }
+  handleClassChange(){
+    this.checkIfNotSingle() ?
+    this.formData.controls.products.patchValue(this.productTypes):
+    this.formData.controls.products.patchValue([]);
+  }
 
-    public compareFn(a: any, b: any): boolean {
-      return a == b;
-    }
+  public compareFn(a: any, b: any): boolean {
+    return a == b;
+  }
 
 
 }
